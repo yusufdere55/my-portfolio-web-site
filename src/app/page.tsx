9a89fdd846d024aa43775b06d144525c2b192ab3
yusufdere55 <yusuf55dere@gmail.com>
2025-05-24 19:02:15 +0300

    Homepage change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <div>
-
+    <div className="flex flex-col px-16  items-center justify-start">
+      <section className="w-full justify-between items-center flex flex-row h-80 py-4 px-8">
+        <div className="flex flex-col justify-center items-start gap-8 ">
+            <h2 className="font-sans text-white/75 font-bold text-6xl">Hi, I'm a <br />Web Developer</h2>
+            <a href="#" className="px-4 font-sans font-semibold py-2 bg-white/5 border transition-all border-white/20 rounded hover:bg-white/20">View My Work</a>
+        </div>
+        <div className="flex justify-center items-center px-8">
+          <span className="text-9xl font-mono font-extrabold text-white/65">&lt;/&gt;</span>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,14 @@
 export default function Navbar() {
     return(
-        <div className="h-20 border-b border-white/50 flex flex-row justify-between px-4 items-center">
-            <h2>Logo</h2>
-            <div className="flex flex-row gap-4 justify-center items-center">
-                
+        <div className="flex border-b border-white/15 flex-row justify-between px-12 items-center h-16">
+            <div>
+                <h1 className="font-bold font-mono text-2xl">YuD3</h1>
+            </div>
+            <div className="flex flex-row gap-4">
+                <div className="font-mono font-semibold"><a href="#">Skills</a></div>
+                <div className="font-mono font-semibold"><a href="#">Projects</a></div>
+                <div className="font-mono font-semibold"><a href="#">About Me</a></div>
+                <div className="font-mono font-semibold"><a href="#">Contact</a></div>
             </div>
         </div>
     )
